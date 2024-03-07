95321b9f9220dbd3c864484dd796ee0917c3466c
Tolga BERBER <tolga.berber@fen.ktu.edu.tr>
2024-03-07 09:14:38 +0300

    Improve performance measurement and space efficiency
    Added dynamic timing for performance measurement at different stages of the code. Refactored repetitive logic to improve readability and space efficiency. Also, introduced Regular Expressions (re module) to simplify multiple space removal logic.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Wikipedi dosyasındaki text bilgisine erişilmeli!
-
+from datetime import datetime
 # DENEME 2
 # JSON görünüyor, Python JSON kütüphanesi ile bilgi almayı deneyelim
 
@@ -7,6 +7,8 @@ from json import loads
 
 # loads bir metin bilgisini Python nesnesine dönüştürür
 print("1- Veri Yükleme")
+genel_baslangic = datetime.now()
+baslangic = genel_baslangic
 orneklem = []
 
 with open("veri/orneklem.json", "r") as dosya:
@@ -14,10 +16,13 @@ with open("veri/orneklem.json", "r") as dosya:
         ornek = loads(satir)
         orneklem.append(ornek)
 
-print(f"Örneklem sayısı {len(orneklem)}")
+print(f"Örneklem sayısı {len(orneklem)} tamamlanma süresi: {datetime.now() - baslangic}")
 
 # Doküman Tanımı
+print("1.1- Doküman Ayıklama")
+baslangic = datetime.now()
 orneklem = [ornek["text"] for ornek in orneklem]
+print(f"Ayıklanan doküman sayısı {len(orneklem)} tamamlanma süresi: {datetime.now() - baslangic}")
 
 # Metin Önişleme
 
@@ -25,6 +30,7 @@ orneklem = [ornek["text"] for ornek in orneklem]
 print("2- Ön işleme")
 
 print("2.1- Karakter Önişleme")
+baslangic = datetime.now()
 # Karakter dönüşümü
 # Python str-> lower(): küçük harfe çevirir, upper(): büyük harfe çevirir
 
@@ -33,6 +39,7 @@ print("2.1- Karakter Önişleme")
 orneklem = [metin.lower() for metin in orneklem]
 
 import unicodedata
+import re
 
 # Unicode Normalizasyonu
 # ö: 1-ö, 2-o ..
@@ -45,40 +52,47 @@ secilen_kategoriler = ['Ll', 'Nd', 'Zs']
 
 for i, ornek in enumerate(orneklem):
     kategoriler = [unicodedata.category(karakter) for karakter in ornek]
-    yeni_metin = []
-    for j, kategori in enumerate(kategoriler):
-        if kategori in secilen_kategoriler:
-            if kategori == 'Zs':
-                yeni_metin.append(' ')
-            else:
-                yeni_metin.append(ornek[j])
-        else:
-            yeni_metin.append(' ')
-    yeni_metin = "".join(yeni_metin)  # listeyi metne geri çevir
+    yeni_metin = "".join([ornek[j] if kategoriler[j] in secilen_kategoriler and kategoriler[j] != 'Zs'
+                          else ' ' for j in range(len(ornek))])
+    # yeni_metin = []
+    # for j, kategori in enumerate(kategoriler):
+    #     if kategori in secilen_kategoriler:
+    #         if kategori == 'Zs':
+    #             yeni_metin.append(' ')
+    #         else:
+    #             yeni_metin.append(ornek[j])
+    #     else:
+    #         yeni_metin.append(' ')
+    # yeni_metin = "".join(yeni_metin)  # listeyi metne geri çevir
     # Yeni problem: Çoklu boşluk problemi
-    while True:
-        uzunluk = len(yeni_metin)
-        yeni_metin = yeni_metin.replace("  ", " ")
-        if len(yeni_metin) == uzunluk:
-            break
+
+    yeni_metin = re.sub(' +', ' ', yeni_metin)
+    # while True:
+    #     uzunluk = len(yeni_metin)
+    #     yeni_metin = yeni_metin.replace("  ", " ")
+    #     if len(yeni_metin) == uzunluk:
+    #         break
     # En iyi alternatif REGEX
-    orneklem[i] = yeni_metin
+    orneklem[i] = yeni_metin.strip()
+
+print(f"Tamamlanma süresi: {datetime.now() - baslangic}")
 
 print("2.2- Metin Parçalama")
+baslangic = datetime.now()
 
 # for i, ornek in enumerate(orneklem):
 #    orneklem[i] = ornek.split(' ')
 orneklem = [ornek.split(' ') for ornek in orneklem]
+print(f"Tamamlanma süresi: {datetime.now() - baslangic}")
 
 print("2.3- Metin Parçası Önişleme")
+baslangic = datetime.now()
 
 import nltk
 
 budayici = nltk.stem.SnowballStemmer('english')
 
 zamirler = nltk.corpus.stopwords.words('english')
-
-print(zamirler)
 
 # for ornek in orneklem:
 #    zamirsiz_ornek = [parca for parca in ornek if parca not in zamirler]
@@ -88,23 +102,26 @@ print(zamirler)
 #    orneklem[i] = [budayici.stem(parca) for parca in ornek if parca not in zamirler]
 
 orneklem = [[budayici.stem(parca) for parca in ornek if parca not in zamirler] for ornek in orneklem]
+print(f"Tamamlanma süresi: {datetime.now() - baslangic}")
 
 print("3- Sayısallaştırma")
 
 print("3.1- Sözlük Oluşturma")
+baslangic = datetime.now()
 sozluk = set()
 for ornek in orneklem:
     # for kelime in ornek:
     #     if kelime not in sozluk:
     #         sozluk.append(kelime)
     sozluk.update(ornek)
-    print("Sozluk boyutu: ", len(sozluk))
 
 sozluk = list(sozluk)
 
 sozluk.sort()
+print(f"Sözlük boyutu {len(sozluk)} tamamlanma süresi: {datetime.now() - baslangic}")
 
 print("3.2- Sayısallaştırma")
+baslangic = datetime.now()
 
 # Kelimeden Sıra Numarasına gitme
 import bisect
@@ -115,9 +132,13 @@ for ornek in orneklem:
     sayisal_ornek = [bisect.bisect_left(sozluk, kelime) for kelime in ornek]
     sayisal_orneklem.append(sayisal_ornek)
 
-print(orneklem[0])
-print(sayisal_orneklem[0])
-d0 = sayisal_orneklem[0]
+print(f"Tamamlanma süresi: {datetime.now() - baslangic}")
+
+print("3.2.1- Doküman Frekansaları Hesaplanıyor")
+baslangic = datetime.now()
 from collections import Counter
 
-frekanslar = Counter(d0)
+frekans_orneklem = [Counter(dokuman) for dokuman in sayisal_orneklem]
+print(f"Tamamlanma süresi: {datetime.now() - baslangic}")
+
+print(f"Genel Tamamlanma süresi : {datetime.now() - genel_baslangic}")
